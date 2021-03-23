81f42c97dab7d1ca83ed0897089b21bf553d0cd6
79120 <katya-student.yrgy@mail.ru>
2021-03-23 21:55:48 +0500

    Задание №26.1:Проверки списков проектов через SOAP

--- fixture/soap.py
+++ fixture/soap.py
@@ -2,13 +2,16 @@ from model.project import Project
 from suds.client import Client
 from suds import WebFault
 
+
 class SoapHelper:
 
-    def __init__(self, app):
+    def __init__(self, app, config):
         self.app = app
+        self.config = config
 
     def can_login(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.config["mantis"]["url"])
+
         try:
             client.service.mc_login(username, password)
             return True
@@ -16,7 +19,7 @@ class SoapHelper:
             return False
 
     def get_projects_list(self, username, password):
-        client = Client("http://localhost/mantisbt-1.2.20/api/soap/mantisconnect.php?wsdl")
+        client = Client(self.config["mantis"]["url"])
         items = []
         # try:
         soap_items = client.service.mc_projects_get_user_accessible(username, password)
